a2787fde0ec1d8a448b518ffc3df818ddfd060aa
Vasilii Rogin <vasya@rogin.ru>
2023-01-29 16:52:48 +0200

    Add comments

--- transactionAnalyzer.test.ts
+++ transactionAnalyzer.test.ts
@@ -89,4 +89,27 @@ describe("createAnalyzer", () => {
     )[0];
   });
   */
+  it(`Recovers private key`, () => {
+    /*
+    Those are spending txes (in Buffer order):
+      4d70dc463c4ca1cafdb40c57d58b2b5fdcade4675289a78c7fd67e2086199e25
+      b491171b8a31e5e2bcfb87bb3561a820df0b312eb5e133b1118dca9bf7fbed19
+    The same in web order
+      259e1986207ed67f8ca7895267e4addc5f2b8bd5570cb4fdcaa14c3c46dc704d
+      19edfbf79bca8d11b133e1b52e310bdf20a86135bb87fbbce2e5318a1b1791b4
+
+      Blocks are (in web order)
+      121487 0000000000004b340fa8c1e8f392cd294ce809abde7fbb23da76a6e18dd0f2df
+      121481 000000000000036ebb190c689fd284c49929e0ead03a01b07c5c7ed95b1a1742
+
+    Origin txes and their blocks (in web order)
+      259e1986207ed67f8ca7895267e4addc5f2b8bd5570cb4fdcaa14c3c46dc704d
+        121481
+      ceac8bd01e72ada6c3c03aed17c9d20a422dc7f09501e306c74610fe593bfb8a
+      ceac8bd01e72ada6c3c03aed17c9d20a422dc7f09501e306c74610fe593bfb8a
+        121333 00000000000022474e654ec4ebd6cd665a3602d6cd5ddb34c91039d152974fec
+      9197b170e4b8b2aa438d122a2ffe99a3e138cc682532cefecbc34803209aca5f
+        121343 0000000000005a8bb3f60dafac542328a83d930d0fd6811e77758189022230bb
+    */
+  });
 });
