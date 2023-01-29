44a08e46de343129881fee9e148b0a529e415c24
Vasilii Rogin <vasya@rogin.ru>
2023-01-29 11:07:43 +0200

    Remove old code

--- my-elliptic-curves/curves.ts
+++ my-elliptic-curves/curves.ts
@@ -111,19 +111,3 @@ export function get_point_inverse(point: Point, curve_p: bigint): Point {
   }
   return [point[0], curve_p - point[1]];
 }
-
-export function uncompressPublicKey(curve: CurveParams, publicKey: Buffer) {
-  const first = publicKey[0];
-  if (first === 0x04) {
-    if (publicKey.length !== 32 + 32 + 1) {
-      throw new Error(`Wrong length!`);
-    }
-    const x = BigInt("0x" + publicKey.subarray(1, 1 + 32).toString("hex"));
-    const y = BigInt(
-      "0x" + publicKey.subarray(1 + 32, 1 + 32 + 32).toString("hex")
-    );
-    if (!is_on_curve([x, y], curve.a, curve.b, curve.p)) {
-      throw new Error(`Not on the curve`);
-    }
-  }
-}
