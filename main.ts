572824ebed1f7b3a382b7d72e27b2a130e94eb34
Vasilii Rogin <vasya@rogin.ru>
2023-02-01 21:48:37 +0200

    Add more seed nodes

--- main.ts
+++ main.ts
@@ -3,4 +3,8 @@ import "dotenv-defaults/config";
 import { createBitcoinNode } from "./bitcoin.node/node";
 
 // seed.bitcoin.sipa.be:8333
-const node = createBitcoinNode([["95.216.21.47", 8333]]);
+const node = createBitcoinNode([
+  ["95.216.21.47", 8333],
+  ["seed.bitcoin.sipa.be", 8333],
+  ["seed.bitcoin.sipa.be", 8333],
+]);
