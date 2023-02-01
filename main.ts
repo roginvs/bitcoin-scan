e152a7df1bda60a8762384019b33ef8b5e633554
Vasilii Rogin <vasya@rogin.ru>
2023-02-01 21:52:14 +0200

    Update seed ip

--- main.ts
+++ main.ts
@@ -5,6 +5,6 @@ import { createBitcoinNode } from "./bitcoin.node/node";
 // seed.bitcoin.sipa.be:8333
 const node = createBitcoinNode([
   ["95.216.21.47", 8333],
-  ["seed.bitcoin.sipa.be", 8333],
-  ["seed.bitcoin.sipa.be", 8333],
+  ["95.216.47.4", 8333],
+  ["95.216.76.224", 8333],
 ]);
