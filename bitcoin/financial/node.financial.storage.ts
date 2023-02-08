8ae5c81b0263bc5f59249d5e46297e6e66144022
Vasilii Rogin <vasya@rogin.ru>
2023-02-08 17:29:57 +0200

    Add draft for financial db

--- bitcoin/financial/node.financial.storage.ts
+++ bitcoin/financial/node.financial.storage.ts
@@ -1,209 +1,74 @@
 import Database from "better-sqlite3";
 import {
+  BlockHash,
   PkScript,
   TransactionHash,
 } from "../../bitcoin/protocol/messages.types";
 import { Nominal } from "../../nominal_types/nominaltypes";
+import { getDbPath } from "../config";
 
-function getDbPath(dbFileName: string) {
-  const dataFolder = process.env.SCANNER_STORAGE_DIR;
-  if (!dataFolder) {
-    throw new Error(`Env variable SCANNER_STORAGE_DIR is not defined`);
-  }
-  return dataFolder + "/" + dbFileName;
+interface UnspentTxRow {
+  transaction_hash: TransactionHash;
+  output_id: number;
+  pub_script: Buffer;
+  /** Or maybe bigint? */
+  value: number;
 }
-
-export type UnspentTxId = Nominal<"unspent tx id", number>;
-
-export interface TransactionRow {
-  compressed_public_key: Buffer;
-  msg: Buffer;
-  r: Buffer;
-  s: Buffer;
-  //spending_tx_hash: TransactionHash;
-  //spending_tx_input_index: number;
-}
-
-export function createTransactionsStorage(isMemory = false) {
-  const sql = new Database(isMemory ? ":memory:" : getDbPath("/scanner.db"));
+export function createFinancialStorage(isMemory = false) {
+  const sql = new Database(isMemory ? ":memory:" : getDbPath("financial.db"));
 
   sql.pragma("journal_mode = WAL");
   sql.pragma("auto_vacuum = FULL");
   sql.exec(`
-  CREATE TABLE IF NOT EXISTS unspent_transaction_output  (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    transaction_hash CHARACTER(32) NOT NULL, 
-    output_id INTEGER NOT NULL,
-    pub_script CHARACTER(32) NOT NULL
-  );
-  -- TODO: Maybe it will be better to remove output_id from this index
-  CREATE INDEX IF NOT EXISTS  unspent_hash_out ON unspent_transaction_output
-    (transaction_hash, output_id);
+    CREATE TABLE IF NOT EXISTS unspent_transaction_outputs  (
+      id INTEGER PRIMARY KEY, 
+      transaction_hash CHARACTER(32) NOT NULL, 
+      output_id INTEGER NOT NULL,
+      pub_script BLOB NOT NULL,
+      value integer
+    );
 
-  CREATE TABLE IF NOT EXISTS signatures  (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    compressed_public_key CHARACTER(33) NOT NULL, 
-    msg CHARACTER (32) NOT NULL,
-    r CHARACTER(32) NOT NULL,
-    s CHARACTER(32) NOT NULL
-    -- spending_tx_hash CHARACTER(32) NOT NULL,
-    -- spending_tx_input_index INTEGER NOT NULL
-  );
-  
-  -- CREATE INDEX IF NOT EXISTS signature_pub_r ON signatures
-  --  (compressed_public_key, r);
-  -- Looks like having index only on r value is faster
-  -- TODO: Check substring(...) index (do we need to update queries too?)
-  DROP INDEX IF EXISTS signature_pub_r;
-  CREATE INDEX IF NOT EXISTS signature_r ON signatures (r);
+    CREATE INDEX IF NOT EXISTS unspent_tx_out_idx ON unspent_transaction_outputs
+     (transaction_hash, output_id);
+
+    -- To get value per each wallet
+    CREATE INDEX IF NOT EXISTS unspent_pub_script_idx ON unspent_transaction_outputs
+     (pub_script);
 
 
-
-
-  CREATE TABLE IF NOT EXISTS found_keys (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    compressed_public_key CHARACTER(33) NOT NULL, 
-    bitcoin_wallet_comp CHARACTER(40), -- Have no idea about hard limit
-    bitcoin_wallet_uncomp CHARACTER(40), -- Have no idea about hard limit
-    private_key CHARACTER(32) NOT NULL,
-    tx_hash_reversed CHARACTER(32) NOT NULL
-  );
-  CREATE UNIQUE INDEX IF NOT EXISTS found_keys_pub_key ON found_keys
-    (compressed_public_key);
+    -- Just a way to remember which block we processed last time
+    CREATE TABLE last_processed_block_hash (
+        _uniq INTEGER NOT NULL CHECK (_uniq = 1),
+        block_hash CHARACTER(32) NOT NULL, 
+    );
+    CREATE UNIQUE INDEX IF NOT EXISTS last_processed_block_hash_idx ON last_processed_block_hash
+     (_uniq);
 `);
 
-  // Use
-  // .mode quote
-  // to show values in sqlite console
-
-  const FIX_EXISTING_DUPLICATES = false;
-  if (FIX_EXISTING_DUPLICATES) {
-    sql.exec(`
-          delete from signatures where id in (
-           select id from signatures group by compressed_public_key, r,s, msg having count(*) > 1
-          );
-        `);
+  function addBlockData(data: {
+    unspentTxesToRemove: [txid: TransactionHash, out_number: number][];
+    addNewUnspentTxes: UnspentTxRow;
+    blockId: BlockHash;
+  }) {
+    // TODO
   }
 
-  const insertSql = sql.prepare(`insert into unspent_transaction_output 
-    (transaction_hash, output_id, pub_script) values (?, ?, ?)`);
-  function addUnspentTxOutput(
-    hash: TransactionHash,
-    output_id: number,
-    pubScript: PkScript
-  ) {
-    insertSql.run(hash, output_id, pubScript);
-  }
-
-  const getUnspentSql = sql.prepare(
-    `
-      select id, pub_script from unspent_transaction_output 
-      where transaction_hash = ? and output_id = ?
-    `
-  );
-  function getUnspentOutput(hash: TransactionHash, output_id: number) {
-    return getUnspentSql.get(hash, output_id) as
-      | { id: UnspentTxId; pub_script: PkScript }
-      | undefined;
-  }
-
-  const removeUnpendTx = sql.prepare(
-    `
-       delete from unspent_transaction_output 
-      where id = ?
-    `
-  );
-  function removeUnspendTx(id: UnspentTxId) {
-    removeUnpendTx.run(id);
-  }
-
-  const saveSignatureSql = sql.prepare(`
-    insert into signatures (        
-        compressed_public_key,
-        msg,
-        r,
-        s
-        -- spending_tx_hash,
-        -- spending_tx_input_index
-      ) values (?, ?, ?, ?)
+  const getUnspentTxSql = sql.prepare(`
+     select transaction_hash, output_id, pub_script, value from
+     unspent_transaction_outputs where transaction_hash = ? and output_id = ?
   `);
-
-  const getSignaturesSql = sql.prepare(`
-    select  
-      compressed_public_key,
-      msg,
-      r,
-      s
-      -- spending_tx_hash,
-      -- spending_tx_input_index
-    from signatures
-    where compressed_public_key = ? and r = ?
- 
-  `);
-
-  function getSignatures(compressed_public_key: Buffer, r: Buffer) {
-    return getSignaturesSql.all(compressed_public_key, r) as TransactionRow[];
+  function getUnspentTx(txid: TransactionHash, output_id: number) {
+    getUnspentTxSql.get(txid, output_id) as UnspentTxRow | undefined;
   }
 
-  function saveSignature(
-    compressed_public_key: Buffer,
-    msg: Buffer,
-    r: Buffer,
-    s: Buffer
-  ) {
-    saveSignatureSql.run(
-      compressed_public_key,
-      msg,
-      r,
-      s
-      // spending_tx_hash,
-      // spending_tx_input_index
-    );
-  }
-
-  function doWeHavePrivateKeyForThisPubKey(publicKey: Buffer) {
-    return !!sql
-      .prepare(`select id from found_keys where compressed_public_key = ?`)
-      .get(publicKey);
-  }
-  /**
-   *
-   */
-  function savePrivateKey(
-    compressed_public_key: Buffer,
-    walletComp: string,
-    walletUncomp: string,
-    privateKey: Buffer,
-    tx_hash_reversed: Buffer
-  ) {
-    sql
-      .prepare(
-        `
-      insert into found_keys (
-        compressed_public_key,
-        bitcoin_wallet_comp,
-        bitcoin_wallet_uncomp,
-        private_key,
-        tx_hash_reversed
-      ) values (?, ?, ?, ?, ?)
-      `
-      )
-      .run(
-        compressed_public_key,
-        walletComp,
-        walletUncomp,
-        privateKey,
-        tx_hash_reversed
-      );
+  function getLastProcessedBlockId() {
+    return sql.prepare("select block_hash from last_processed_block_hash").get()
+      ?.block_hash as BlockHash | undefined;
   }
 
   return {
-    addUnspentTxOutput,
-    getUnspentOutput,
-    removeUnspendTx,
-    getSignatures,
-    saveSignature,
-    doWeHavePrivateKeyForThisPubKey,
-    savePrivateKey,
+    addBlockData,
+    getUnspentTx,
+    getLastProcessedBlockId,
   };
 }
