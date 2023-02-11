8f95dbcdbcb5e760b588b71bbf5fc97ccab34bf5
Vasilii Rogin <vasya@rogin.ru>
2023-02-11 09:26:07 +0200

    Scanner is using new API

--- scanner/main.ts
+++ scanner/main.ts
@@ -2,59 +2,20 @@ import "dotenv-defaults/config";
 import { createBitcoinNode, BlockId } from "../bitcoin/node";
 import { BitcoinBlock } from "../bitcoin/protocol/messages.parse";
 import { createLogger } from "../logger/logger";
-import { createAnalyzer } from "./signaturesAnalyzer";
+import { createSignaturesAnalyzer } from "./signaturesAnalyzer";
 const { info, warn, debug } = createLogger("SCANNER");
 
-const analyzer = createAnalyzer();
+const analyzer = createSignaturesAnalyzer();
 debug("Analyzer created");
 
 const node = createBitcoinNode();
 
-let totalKeysFound = 0;
-function processBlock(block: BitcoinBlock, currentHeight: number) {
-  const started = new Date();
-  const blockInformation = Buffer.from(block.hash).reverse().toString("hex");
-  let savedOutputsCount = 0;
-  let savedSignatures = 0;
-  let keysFound = 0;
-  for (const tx of block.transactions) {
-    const stats = analyzer.transaction(tx, blockInformation);
-    savedOutputsCount += stats.savedOutputsCount;
-    savedSignatures += stats.savedSignatures;
-    keysFound += stats.keysFound;
-  }
-  totalKeysFound += keysFound;
+node.onValidatedSignature((sigInfo) => analyzer.ecdsaSignature(sigInfo));
+node.onAfterBlockSaved(() => {
   info(
-    `block=...${blockInformation.slice(-8)} height=${currentHeight} ` +
-      `tx_count=${block.transactions.length} savedOutputsCount=${savedOutputsCount} ` +
-      `savedSignatures=${savedSignatures} ` +
-      `took ${new Date().getTime() - started.getTime()}ms, ` +
-      `keysFound=${keysFound} totalKeysFound=${totalKeysFound}`
+    `signaturesSaved=${analyzer.signaturesSaved} keysFound=${analyzer.keysFound}`
   );
-
-  if (keysFound > 0) {
-    warn(`FOUND NEW KEYS`);
-  }
-}
-
-const RESCAN_EVERYTHING_FROM_THE_BEGINNING = false;
-if (RESCAN_EVERYTHING_FROM_THE_BEGINNING) {
-  let i = 1;
-  while (true) {
-    const block = node.getSavedBlock(i as BlockId)?.[0];
-    if (!block) {
-      break;
-    }
-    if (block.transactions.length === 0) {
-      break;
-    }
-    processBlock(block, i - 1);
-
-    i++;
-  }
-}
-
-node.onBeforeBlockSaved(processBlock);
+});
 
 const pruning = process.env.SCANNER_PRUNE
   ? parseInt("process.env.SCANNER_PRUNE")
