c1657b2ae175223ea272ed1efa6b3a821abd35b3
Vasilii Rogin <vasya@rogin.ru>
2023-02-11 11:16:03 +0200

    Add base58 decode

--- bitcoin/utils/base58.test.ts
+++ bitcoin/utils/base58.test.ts
@@ -1,17 +1,22 @@
-import { base58encode, bitcoinAddressFromP2PKH } from "./base58";
+import { base58decode, base58encode, bitcoinAddressFromP2PKH } from "./base58";
 
+const testData = {
+  "0000030405": "1121kY",
+  "009C13ABEAA29473787191861F62952F651CE6EDAC3D6CC5B3":
+    "1FEFyjGTFbc128EXz298mRd2PsPGhobkWe",
+};
 describe(`base58encode`, () => {
-  const testData = {
-    "0000030405": "1121kY",
-    "009C13ABEAA29473787191861F62952F651CE6EDAC3D6CC5B3":
-      "1FEFyjGTFbc128EXz298mRd2PsPGhobkWe",
-  };
   for (const [input, output] of Object.entries(testData)) {
     it(`Encodes ${input} into ${output}`, () =>
       expect(base58encode(Buffer.from(input, "hex"))).toBe(output));
   }
 });
-
+describe(`base58decode`, () => {
+  for (const [output, input] of Object.entries(testData)) {
+    it(`Decodes ${input} into ${output}`, () =>
+      expect(base58decode(input).toString("hex").toUpperCase()).toBe(output));
+  }
+});
 describe(`Encode address`, () => {
   const testData = {
     "9c13abeaa29473787191861f62952f651ce6edac":
--- bitcoin/utils/base58.ts
+++ bitcoin/utils/base58.ts
@@ -1,10 +1,8 @@
 import { sha256 } from "./hashes";
 import { joinBuffers } from "./joinBuffer";
+const codeString = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
 export function base58encode(data: Buffer) {
-  const codeString =
-    "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-
   let output = "";
   let x = BigInt(`0x` + data.toString("hex"));
   while (x > BigInt(0)) {
@@ -26,4 +24,26 @@ export function bitcoinAddressFromP2PKH(pubKeyHash: Buffer) {
 
   const base256 = joinBuffers(withNetworkId, hash.subarray(0, 4));
   return base58encode(base256);
+}
+
+export function base58decode(data: string) {
+  let out = BigInt(0);
+  for (const [pos, digit] of data.split("").reverse().entries()) {
+    const digitNum = codeString.indexOf(digit);
+    if (digitNum < 0) {
+      throw new Error(`Wrong base58 char ${digit}`);
+    }
+    out = out + BigInt(digitNum) * BigInt(58) ** BigInt(pos);
+  }
+  let outStr = out.toString(16);
+  if (outStr.length % 2 != 0) {
+    outStr = "0" + outStr;
+  }
+
+  let i = 0;
+  while (i < data.length && data.charAt(i) === "1") {
+    outStr = "00" + outStr;
+    i++;
+  }
+  return Buffer.from(outStr, "hex");
 }
