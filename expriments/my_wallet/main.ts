ade04e4ede605a54e2ad560ea8dd8732bb583313
Vasilii Rogin <vasya@rogin.ru>
2023-02-16 19:51:12 +0200

    Add comments

--- expriments/my_wallet/main.ts
+++ expriments/my_wallet/main.ts
@@ -1,7 +1,11 @@
 /*
 
 # First, generate private key
-
 openssl ecparam -genkey -out wallet.pem -name secp256k1 -noout
+
+# Encrypt and save somewhere
+openssl aes-256-cbc -e -pbkdf2 -in wallet.pem -out wallet.enc.pem
+# Decrypt
+openssl aes-256-cbc -d -pbkdf2 -in wallet.enc.pem -out wallet.decccc.pem
 
 */
