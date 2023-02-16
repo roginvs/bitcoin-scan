2d2e25c05a6c09a7eee86be193229203273a8cfc
Vasilii Rogin <vasya@rogin.ru>
2023-02-16 21:12:36 +0200

    Add tx size calculator

---
+++ bitcoin/financial/txsize.ts
@@ -0,0 +1,20 @@
+import { packTx } from "../protocol/messages.create";
+import { BitcoinTransaction, readTx } from "../protocol/messages.parse";
+import { TransactionPayload } from "../protocol/messages.types";
+
+export function getTxSize(tx: TransactionPayload | BitcoinTransaction) {
+  const txFullRaw = tx instanceof Buffer ? tx : packTx(tx);
+  const txNoWitness = packTx({
+    ...(tx instanceof Buffer ? readTx(tx)[0] : tx),
+    isWitness: false,
+  });
+  const size =
+    tx instanceof Buffer
+      ? tx.length
+      : tx.isWitness
+      ? txFullRaw.length
+      : txNoWitness.length;
+  const weight =
+    txNoWitness.length * 4 + (txFullRaw.length - txNoWitness.length);
+  return { size, weight };
+}
