675426f47d08aa217129f54d90d69efdfac16e03
Vasilii Rogin <vasya@rogin.ru>
2023-02-16 19:59:33 +0200

    Add more experiments

--- expriments/my_wallet/main.ts
+++ expriments/my_wallet/main.ts
@@ -9,3 +9,39 @@ openssl aes-256-cbc -e -pbkdf2 -in wallet.pem -out wallet.enc.pem
 openssl aes-256-cbc -d -pbkdf2 -in wallet.enc.pem -out wallet.decccc.pem
 
 */
+
+import { createPrivateKey, createPublicKey } from "crypto";
+import * as fs from "fs";
+import { compressPublicKey } from "../../bitcoin/protocol/compressPublicKey";
+import { asn1parse } from "../../bitcoin/script/asn1";
+import { bitcoinAddressP2WPKHromPublicKey } from "../../bitcoin/utils/bech32/address";
+
+const myPrivKeyObject = createPrivateKey({
+  key: fs.readFileSync(__dirname + "/wallet.pem"),
+  format: "pem",
+  type: "sec1",
+});
+const myPublicKeyObject = createPublicKey(myPrivKeyObject);
+
+const myPublicKey = (() => {
+  const myPublicKeySpki = myPublicKeyObject.export({
+    format: "der",
+    type: "spki",
+  });
+  const asnParsed = asn1parse(myPublicKeySpki);
+  // .subarray(20 + 2 + 1, 20 + 2 + 1 + 66);
+  const myPublicKeyUncompressed = asnParsed[0][1].value;
+  if (
+    myPublicKeyUncompressed[0] !== 0x04 ||
+    myPublicKeyUncompressed.length !== 65
+  ) {
+    throw new Error(
+      `Something wrong or crypto module decided to compress public key`
+    );
+  }
+  return compressPublicKey(myPublicKeyUncompressed)!;
+})();
+
+console.info("Public key = ", myPublicKey.toString("hex"));
+const p2wpkh = bitcoinAddressP2WPKHromPublicKey(myPublicKey);
+console.info(`Address = ${p2wpkh}`);
