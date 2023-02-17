644864fe687807ad0b08dba2df407d2ced2cbd28
Vasilii Rogin <vasya@rogin.ru>
2023-02-17 04:23:45 +0200

    First transaction on air

--- expriments/my_wallet/main.ts
+++ expriments/my_wallet/main.ts
@@ -1,5 +1,6 @@
 import "dotenv-defaults/config";
 import { createBitcoinBlocksNode } from "../../bitcoin/blockchain/blockchain.node";
+import { createTransaction } from "./createTx";
 
 const node = createBitcoinBlocksNode();
 
@@ -7,3 +8,12 @@ process.on("SIGINT", () => {
   console.info("Received SIGINT, terminating");
   node.stop();
 });
+
+const demoTx = createTransaction();
+console.info(
+  `Transaction id = ${Buffer.from(demoTx.parsed.txid)
+    .reverse()
+    .toString("hex")}`
+);
+console.info(demoTx.parsed);
+node.addTxToMempool(demoTx.parsed);
