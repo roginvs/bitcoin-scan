488e48803d415103709b36a54f634de9403fedda
Vasilii Rogin <vasya@rogin.ru>
2023-02-22 20:37:39 +0200

    Update failing test

--- signature/stringToUTF8Array.test.ts
+++ signature/stringToUTF8Array.test.ts
@@ -18,9 +18,25 @@ describe(`stringToUTF8Array`, () => {
       Buffer.from(wildString).toString("hex")
     );
   });
-  it(`Wild string 2`, () => {
+  it.only(`Wild string 2 (1char)`, () => {
     const wildString = String.fromCharCode(
       ...new Array(1).fill(0).map((_, i) => i + 55296)
+    );
+    expect(Buffer.from(stringToUTF8Array(wildString)).toString("hex")).toBe(
+      Buffer.from(wildString).toString("hex")
+    );
+  });
+  it(`Wild string 2`, () => {
+    const wildString = String.fromCharCode(
+      ...new Array(57343 - 55296 + 1).fill(0).map((_, i) => i + 55296)
+    );
+    expect(Buffer.from(stringToUTF8Array(wildString)).toString("hex")).toBe(
+      Buffer.from(wildString).toString("hex")
+    );
+  });
+  it(`Wild string 3`, () => {
+    const wildString = String.fromCharCode(
+      ...new Array(70000 - 57343).fill(0).map((_, i) => i + 57343 + 1)
     );
     expect(Buffer.from(stringToUTF8Array(wildString)).toString("hex")).toBe(
       Buffer.from(wildString).toString("hex")
