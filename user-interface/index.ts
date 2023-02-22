a2c6e1ec4ec686dbcaeada413704810df12d73fa
Vasilii Rogin <vasya@rogin.ru>
2023-02-22 22:07:02 +0200

    Ready to use it on frontend

--- user-interface/index.ts
+++ user-interface/index.ts
@@ -1,3 +1,17 @@
-import { sha256 } from "../my-hashes/sha256";
-console.info(`It works`);
-console.info(sha256(new ArrayBuffer(0)));
+import { signatureToPublicKey } from "../signature/signatureToPublicKey";
+import { pubkeyToWallet } from "./pubkeyToWallet";
+
+console.info(`Let's start`);
+
+const pubKey = signatureToPublicKey(`
+-----BEGIN BITCOIN SIGNED MESSAGE-----
+lol kek asdasd
+-----BEGIN BITCOIN SIGNATURE-----
+ILTEzkSmm6R9ElES2zSbclDyej7J2kc3SQzwMINw4xlMcTIXHUL6d07/Vp3rL83OXTEE4GepulkfdxYCCg5eNBI=
+-----END BITCOIN SIGNATURE-----
+`);
+if (!pubKey) {
+  throw new Error("Failed");
+}
+const wallet = pubkeyToWallet(pubKey?.pubKeyHex, pubKey?.walletType);
+console.info(wallet);
