491a66f849265e984e0fcca1865b964790858b4a
Vasilii Rogin <vasya@rogin.ru>
2023-02-23 22:42:36 +0200

    Experiments with predictable wallets

--- expriments/predictable_wallets/main.ts
+++ expriments/predictable_wallets/main.ts
@@ -1,9 +1,13 @@
 import { PkScript } from "../../bitcoin/protocol/messages.types";
-import { getCompressedPublicKeyFromPrivateKey } from "../../bitcoin/protocol/publicKeyFromPrivateKey";
+import {
+  getCompressedPublicKeyFromPrivateKey,
+  getUnCompressedPublicKeyFromPrivateKey,
+} from "../../bitcoin/protocol/publicKeyFromPrivateKey";
 import {
   bitcoin_address_P2PKH_from_pubkey_hash,
   bitcoin_address_P2PKH_from_public_key,
   bitcoin_address_P2SH_from_pk_script,
+  bitcoin_address_P2SH_from_script_hash,
 } from "../../bitcoin/utils/adresses";
 import {
   bitcoin_address_P2WPKH_from_public_key,
@@ -51,7 +55,12 @@ function getSimplePrivateKeys() {
   return privKeys;
 }
 
-type WalletInfo = [address: string, info: string, secret: string];
+type WalletInfo = [
+  pkScript: PkScript,
+  address: string,
+  info: string,
+  secret: string
+];
 
 function getScriptWallets(
   pkScript: PkScript,
@@ -61,17 +70,46 @@ function getScriptWallets(
   const walletsInfo: WalletInfo[] = [];
 
   {
-    const p2sh = bitcoin_address_P2SH_from_pk_script(pkScript);
-    walletsInfo.push([p2sh, `P2SH${comment}`, secret]);
+    const scriptHash = ripemd160(sha256(pkScript));
+    const p2shAddress = bitcoin_address_P2SH_from_script_hash(scriptHash);
+    const expectingPkScript = Buffer.concat([
+      Buffer.from("a914", "hex"),
+      scriptHash,
+      Buffer.from("87", "hex"),
+    ]) as PkScript;
+    walletsInfo.push([
+      expectingPkScript,
+      p2shAddress,
+      `P2SH${comment}`,
+      secret,
+    ]);
   }
   {
-    const p2wsh = bitcoin_address_P2WSH_from_pk_script(pkScript)!;
-    walletsInfo.push([p2wsh, `P2WSH${comment}`, secret]);
+    const p2wshAddress = bitcoin_address_P2WSH_from_pk_script(pkScript)!;
+
+    const expectingPkScript = get_P2WSH_pk_script_from_real_pk_script(pkScript);
+    walletsInfo.push([
+      expectingPkScript,
+      p2wshAddress,
+      `P2WSH${comment}`,
+      secret,
+    ]);
   }
   {
     const p2wsh_script = get_P2WSH_pk_script_from_real_pk_script(pkScript);
-    const p2sh_p2wsh = bitcoin_address_P2SH_from_pk_script(p2wsh_script);
-    walletsInfo.push([p2sh_p2wsh, `P2SH+P2WSH${comment}`, secret]);
+    const p2wsh_script_hash = ripemd160(sha256(p2wsh_script));
+    const address = bitcoin_address_P2SH_from_script_hash(p2wsh_script_hash);
+    const expectingPkScript = Buffer.concat([
+      Buffer.from("a914", "hex"),
+      p2wsh_script_hash,
+      Buffer.from("87", "hex"),
+    ]) as PkScript;
+    walletsInfo.push([
+      expectingPkScript,
+      address,
+      `P2SH+P2WSH${comment}`,
+      secret,
+    ]);
   }
 
   return walletsInfo;
@@ -82,15 +120,52 @@ function getPublickeyWallets(privKey: Buffer) {
 
   const publicKey = getCompressedPublicKeyFromPrivateKey(privKey);
   const secret = privKey.toString("hex");
+
+  if (publicKey.length !== 33) {
+    throw new Error(`Wrong len for pubkey comp`);
+  }
   {
+    // Pubkey (P2PK) compressed
+    const script = Buffer.concat([
+      Buffer.from([33]),
+      publicKey,
+      Buffer.from("ac", "hex"),
+    ]) as PkScript;
+
+    walletsInfo.push([script, "", `P2PK compressed`, secret]);
+  }
+
+  {
+    // Pubkey (P2PK) uncompressed
+    const pubKeyUncom = getUnCompressedPublicKeyFromPrivateKey(privKey);
+    if (pubKeyUncom.length !== 65) {
+      throw new Error(`Wrong len for pubkey uncomp`);
+    }
+    const script = Buffer.concat([
+      Buffer.from([65]),
+      publicKey,
+      Buffer.from("ac", "hex"),
+    ]) as PkScript;
+
+    walletsInfo.push([script, "", `P2PK compressed`, secret]);
+  }
+  {
+    const keyHash = ripemd160(sha256(publicKey));
     walletsInfo.push([
-      bitcoin_address_P2PKH_from_public_key(publicKey),
+      Buffer.concat([
+        Buffer.from([0x76, 0xa9]),
+        keyHash,
+        Buffer.from([0x88, 0xac]),
+      ]) as PkScript,
+      bitcoin_address_P2PKH_from_pubkey_hash(publicKey),
       `P2PKH`,
       secret,
     ]);
   }
   {
+    const keyHash = ripemd160(sha256(publicKey));
     walletsInfo.push([
+      Buffer.concat([Buffer.from([0x00, keyHash.length]), keyHash]) as PkScript,
       bitcoin_address_P2WPKH_from_public_key(publicKey)!,
       `P2WPKH`,
       secret,
@@ -127,8 +202,6 @@ function getPublickeyWallets(privKey: Buffer) {
       walletsInfo.push(...getScriptWallets(script, ` PK ${ending}`, secret));
     }
   }
-
-  // - p2pk (?? this is not a wallet?)
 
   return walletsInfo;
 }
